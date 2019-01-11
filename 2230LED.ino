dc11485726af1d5f128e77eab2bc92f4ad0447e3
Nadav Tasher <tasher.nadav@gmail.com>
2019-01-11 12:44:10 +0200

    RGB Shit

--- 2230LED.ino
+++ 2230LED.ino
@@ -1,7 +1,28 @@
-void setup() {
+#include <Wire.h>
 
+#define DIN_PIN 4
+
+//SDA on RoboRIO to A4
+//SCL on RoboRIO to A5
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin(4);
+  Wire.onReceive(writeLeds);
 }
 
 void loop() {
 
 }
+
+void writeLeds(int bytes){
+  int ledCount=bytes/7;
+  int colors[ledCount];
+  String current="";
+  while (Wire.available()) {
+    char c = Wire.read();
+    Serial.print(c);
+  }
+  Serial.println();
+}
+
