6b834911382bb9b2a891e23ee30208dba5f4de68
Nadav Tasher <tasher.nadav@gmail.com>
2019-02-08 08:52:16 +0200

    Added Clear

--- 2230LED.ino
+++ 2230LED.ino
@@ -5,24 +5,33 @@
 #define LOOP_DELAY 10 //100Hz
 
 CRGB leds[LEDS];
+CRGB color, current;
 long led;
 byte buffer[3];
 
 void setup() {
-  Serial.begin(9600); 
+  Serial.begin(9600);
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, LEDS);
 }
 
 void loop() {
-  Serial.readBytes(buffer,3);
-  CRGB tempColor = leds[0];
-  for (led = 1; led < LEDS; led++) {
-    CRGB tempCurrent = leds[led];
-    leds[led] = tempColor;
-    tempColor = tempCurrent;
+  Serial.readBytes(buffer, 3);
+  // Check if need to clear
+  if (buffer[2] == -1 && buffer[1] == -1 && buffer[0] == -1) {
+    tempColor = CRGB(0, 0, 0);
+    for (led = 0; led < LEDS; led++) {
+      leds[led] = tempColor;
+    }
+  } else {
+    color = leds[0];
+    for (led = 1; led < LEDS; led++) {
+      current = leds[led];
+      leds[led] = color;
+      color = current;
+    }
+    // BGR Mode
+    leds[0] = CRGB(buffer[2], buffer[1], buffer[0]);
   }
-  // BGR Mode
-  leds[0] = CRGB(buffer[2], buffer[1], buffer[0]);
   FastLED.show();
   delay(LOOP_DELAY);
 }
