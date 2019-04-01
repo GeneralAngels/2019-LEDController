3922e771bba129687569631aac4fb101b5004fbb
Nadav Tasher <tasher.nadav@gmail.com>
2019-04-01 18:40:13 +0300

    New Modes

--- 2230LED.ino
+++ 2230LED.ino
@@ -1,13 +1,13 @@
 #include <FastLED.h>
 
 #define LED_PIN 3
-#define LEDS 69
-#define LOOP_DELAY 10 //100Hz
+#define LEDS 55
+#define LOOP_DELAY 20 //50Hz
 
 CRGB leds[LEDS];
 CRGB color, current;
 long led;
-byte buffer[3];
+byte buffer[5];
 
 void setup() {
   Serial.begin(9600);
@@ -15,22 +15,37 @@ void setup() {
 }
 
 void loop() {
-  Serial.readBytes(buffer, 3);
-  // Check if need to clear
-  if (buffer[2] == -1 && buffer[1] == -1 && buffer[0] == -1) {
-    color = CRGB(0, 0, 0);
-    for (led = 0; led < LEDS; led++) {
-      leds[led] = color;
+  Serial.readBytes(buffer, 5);
+  if ((int)buffer[1] > 0) {
+    if ((int)buffer[0] == 0) {
+      for (led = 0; led < (int)buffer[1]; led++) {
+        leds[led] = CRGB(buffer[4], buffer[3], buffer[2]);
+      }
+    } else {
+      for (led = LEDS - 1; led > LEDS - (int)buffer[1] - 1; led--) {
+        leds[led] = CRGB(buffer[4], buffer[3], buffer[2]);
+      }
     }
   } else {
-    color = leds[0];
-    for (led = 1; led < LEDS; led++) {
-      current = leds[led];
-      leds[led] = color;
-      color = current;
+    if ((int)buffer[0] == 0) {
+      color = leds[0];
+      for (led = 1; led < LEDS; led++) {
+        current = leds[led];
+        leds[led] = color;
+        color = current;
+      }
+      // BGR Mode
+      leds[0] = CRGB(buffer[4], buffer[3], buffer[2]);
+    } else {
+      color = leds[LEDS - 1];
+      for (led = LEDS - 2; led >= 0; led++) {
+        current = leds[led];
+        leds[led] = color;
+        color = current;
+      }
+      // BGR Mode
+      leds[LEDS - 1] = CRGB(buffer[4], buffer[3], buffer[2]);
     }
-    // BGR Mode
-    leds[0] = CRGB(buffer[2], buffer[1], buffer[0]);
   }
   FastLED.show();
   delay(LOOP_DELAY);
